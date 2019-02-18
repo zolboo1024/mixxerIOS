8ce61ffeea6fd7c2fbea927f8cdac9821212e2e0
zolboo1024 <erdenebz@dickinson.edu>
2019-02-18 16:24:22 -0500

    Initial Commit

---
+++ mixxer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mixxer
+//
+//  Created by Erdenebaatar, Zolboo on 2/18/19.
+//  Copyright © 2019 Dickinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
